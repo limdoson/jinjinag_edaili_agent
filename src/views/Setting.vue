e97018a2c13c95bc8544c6f0881b4b2098575fbf
lindusen <181408022@qq.com>
2019-06-26 22:36:12 +0800

    leem 6.26

---
+++ src/views/Setting.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="setting page">
+		<back-header></back-header>
+		<van-cell title="修改密码" is-link to='reset-pwd' />
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
